7abb0193ceed0ab1f01d3c45c77bf9db2d2539f9
nikitapt63 <nikitapt63@gmail.com>
2022-11-21 20:00:03 +0300

    Solve second task. Spin words

--- lesson_8.py
+++ lesson_8.py
@@ -3,5 +3,8 @@ def array_diff(a, b):
     return result
 
 
+def spin_words(sentence: str):
+    result = [word[::-1] if len(word) >= 5 else word for word in sentence.split()]
+    return " ".join(result)
 
 
