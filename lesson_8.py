5ae86413c549a43271e3f0d9dc0a016d4ba563c0
nikitapt63 <nikitapt63@gmail.com>
2022-11-21 22:05:58 +0300

    Lesson 8

--- lesson_8.py
+++ lesson_8.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def array_diff(a, b):
     result = [number for number in a if number not in b]
     return result
@@ -15,4 +18,33 @@ def find_it(seq):
 
 def is_square(n):
     return True if n ** 0.5 == int(n ** 0.5) else False
+
+
+def descending_order(num: int) -> int:
+    int_lst_num = list(map(int, str(num)))
+    int_lst_num.sort(reverse=True)
+    str_lst_num = map(str, int_lst_num)
+    return int("".join(str_lst_num))
+
+
+def move_zeros(lst: list):
+    result = [num for num in lst if num != 0]   # [1, 1, 2, 1, 3]
+    delta = len(lst) - len(result)  # 2
+    for _ in range(1, delta + 1):
+        result.append(0)
+    return result
+
+
+from functools import reduce
+
+
+def digital_root(n):
+    if n <= 9:
+        return n
+    else:
+        return digital_root(reduce(lambda x, y: int(x) + int(y), str(n)))
+
+
+
+
 
